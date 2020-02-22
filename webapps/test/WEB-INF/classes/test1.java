9d7a37964d637860147c6c872a1305a83bf90ee2
watanabesota <watanabesota@192.168.3.5>
2020-02-22 15:39:50 +0900

    OKOKを反映

--- webapps/test/WEB-INF/classes/test1.java
+++ webapps/test/WEB-INF/classes/test1.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 public class test1 extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     PrintWriter out = response.getWriter();
-    out.println("<html>test1 OK</html>");
+    out.println("<html>test1 OKOKOKOKO</html>");
     out.close();
   }
 }
